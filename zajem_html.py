10ae47adf9871bf295fa27afc1ff617ea9c4c75c
Ursa <ursazrimsek@gmail.com>
2018-05-13 14:55:29 +0200

    parsing data from beer files

--- zajem_html.py
+++ zajem_html.py
@@ -1,5 +1,6 @@
 import re
 import orodja
+import os
 
 def zajemi_strani_vino():
     '''Zajame podatke iz spletne strani'''
@@ -41,6 +42,11 @@ regex_url = re.compile(
     r'<a href="(?P<url>.*?)"><img alt="(?P<Ime>.*?)" class="img-full-responsive" .*?',
     flags=re.DOTALL)
 
+regex_url_beer = re.compile(
+    r' <div id=".*?" class=".*?" data-product-url="https://www.masterofmalt.*?'
+    r'.com/beer/(?P<url>.*?)" data-productid="(?P<id>\d{4,8})">.*?',
+    flags=re.DOTALL
+)
 
 def izloci_url(imenik, regex):
     '''Vrne seznam s slovarji podatkov iz imenika'''
@@ -54,13 +60,25 @@ def izloci_url(imenik, regex):
 
 
 def zajemi_vina():
+    '''Pregleda celotne strani in zajame posamezna vina'''
     vina = izloci_url('strani_vina/', regex_url)
     for vino in vina:
         ime_datoteke = 'vina/{}.html'.format(vino['id'])
         orodja.shrani(vino['url'], ime_datoteke)
 
 
+def zajemi_piva():
+    '''Pregleda celotne strani in zajame posamezna piva'''
+    piva = izloci_url('beerpages/', regex_url_beer)
+    for pivo in piva:
+        ime_datoteke = 'beer/{}.html'.format(pivo['id'])
+        url = "https://www.masterofmalt.com/beer/" + pivo['url']
+        orodja.shrani(url, ime_datoteke)
+
+
 regex_vino = re.compile(
+    r'<h1 itemprop=\'name\'.*?title">(?P<Name>.*?)((\$|&).*)?<small>(?P<ShortDes>.*?)</small></h1>.*?'
+    r'<span itemprop=\'price\'>(?P<Price>.*?)</span>.*?'
     r'<p itemprop=\'(description|reviewBody(?!(.*?itemprop=\'description)))\'>"?(?P<Description>.*?)"?(</p>|<br>).*?'
     r'<td class=\'label\'>Item #</td>.*?'
     r'<td class=\'data\'>(?P<id>\d{4,8})</td>.*?'
@@ -71,7 +89,7 @@ regex_vino = re.compile(
     r'<td class="label">Color</td>.*?'
     r'<td class="data"><a href="/search\?color%.*?">(?P<Color>.*?)</a></td>.*?'
     r'<td class="label">ABV</td>.*?'
-    r'<td class="data">(?P<ABV>.*?)%</td>.*?'
+    r'<td class="data">(?P<ABV>.*?)%?</td>.*?'
     r'<td class="data"><a href="/search\?varietal%5B%5D.*?"(?P<Varietal>.*?)</td>.*?'
     r'<td class="label">Size</td>.*?'
     r'<td class="data">(?P<Size>.*?)</td>.*?'
@@ -83,24 +101,73 @@ regex_vino = re.compile(
     r'<td class="data">(?P<Smell>.*?)</td>.*?',
     flags=re.DOTALL)
 
-
 regex_vrsta = re.compile(r'>(?P<vrsta>.*?)</a>')
 
 
-def izloci_podatke_vin(imenik, regex):
+regex_pivo = re.compile(
+    r'(<meta itemprop=\'price\' content=\'(?P<Price>.*?)\'/>|Discontinued).*?'
+    r'<span id="ContentPlaceHolder1_ctl00_ctl00_wdItemNameCountry" class="kv-key gold">Country</span>.*?'
+    r'<span class="kv-val">(?P<Country>.*?)</span>.*?'
+    r'<span id="ContentPlaceHolder1_ctl00_ctl00_wdItemNameDistillery" class="kv-key gold">Brewery</span>.*?'
+    r'<span class="kv-val" itemprop="brand"><a href="https://www.masterofmalt.com/distilleries/.*?/">(?P<Brewery>.*?)</a></span>.*?'
+    r'<span id="ContentPlaceHolder1_ctl00_ctl00_wdItemNameBottler" class="kv-key gold">Bottler</span>.*?'
+    r'<span class="kv-val" itemprop="manufacturer">(?P<Bottler>.*?)</span>.*?'
+    r'<span id="ContentPlaceHolder1_ctl00_ctl00_wdItemNameStyle" class="kv-key gold">Style</span>.*?'
+    r'<span class="kv-val">(?P<Style>.*?)</span>.*?'
+    r'<span id="ContentPlaceHolder1_ctl00_ctl00_wdItemNameAlcohol" class="kv-key gold">Alcohol</span>.*?'
+    r'<span class="kv-val">(?P<ABV>.*?)%</span>.*?'
+    r'<span id="ContentPlaceHolder1_ctl00_ctl00_wdItemNameVolume" class="kv-key gold">Volume</span>.*?'
+    r'<span class="kv-val">(?P<Volume>.*?)</span>.*?'
+    r'var isProductPage = true,.*?lastViewedProductID = \d{4,8},.*?productID = (?P<id>\d{4,8});.*?'
+    r'"name": "(?P<Name>.*?)",.*?"description": "(?P<Description>.*?)",.*?',
+    flags=re.DOTALL)
+
+
+def izloci_podatke_piv(imenik, regex):
+    '''Iz html datotek izloči podatke vin'''
     podatki = []
     for datoteka in orodja.datoteke(imenik):
-        for vino in re.finditer(regex, orodja.vsebina_datoteke(datoteka)):
+        pivo = re.search(regex, orodja.vsebina_datoteke(datoteka))
+        if pivo is not None:
+            pod = pivo.groupdict()
+            if pod['Price'] is None:
+                pod['Price'] = 'Discontinued'
+            else:
+                pod['Price'] = float(pod['Price'])
+            pod['id'] = int(pod['id'])
+            pod['Description'] = str(pod['Description']).replace('\n', '')
+
+            podatki.append(pod)
+        else:
+            print(datoteka)
+    return podatki
+
+
+def izloci_podatke_vin(imenik, regex):
+    '''Iz html datotek izloči podatke vin'''
+    podatki = []
+    for datoteka in orodja.datoteke(imenik):
+        vino = re.search(regex, orodja.vsebina_datoteke(datoteka))
+        if vino is not None:
             pod = vino.groupdict()
+            pod['Price'] = float(pod['Price'])
             pod['id'] = int(pod['id'])
             vrste = regex_vrsta.findall(pod['Varietal'])
             pod['Varietal'] = vrste
             pod['Description'] = str(pod['Description']).replace('\n', '')
-            pod['ABV'] = float(pod['ABV'])
+            if pod['ABV'] != 'N/A':
+                pod['ABV'] = float(pod['ABV'])
+            else:
+                pod['ABV'] = None
             podatki.append(pod)
+        else:
+            print(datoteka)
+            os.remove(datoteka)
     return podatki
 
-podatki = izloci_podatke_vin('vina_manjsa', regex_vino)
-print(podatki)
-print(len(podatki))
+def csv_vina():
+    '''Podatke vin zapiše v csv datoteko'''
+    vina = izloci_podatke_vin('vina', regex_vino)
+    orodja.zapisi_tabelo(vina, ['id', 'Name', 'ShortDes', 'Color', 'Varietal', 'Price', 'Size', 'ABV', 'Country',
+                                'Region', 'Closure', 'Taste', 'Smell', 'Description'], 'CSV/vina.csv')
 
